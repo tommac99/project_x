e0dc38c1c2f1ca1d1074f89a6bed61f4b51916bd
Thomas MacFarlaine <tommacfarlaine@gmail.com>
2021-05-20 15:26:16 +0200

    webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: "development",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js"
+    filename: "index.js"
   },
   devtool: "sourcemap",
   module: {
